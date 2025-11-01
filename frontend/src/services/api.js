0e670c8071b8eb8873c8f60dbe4ff7a6e1aaa948
S H M J N Sooriyahetti <janithnilukshan@gmail.com>
2025-11-01 12:46:07 +0530

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/apiService.js
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL?.replace(/\/$/, "") || "http://apexauto.duckdns.org/api";
 
 class ApiService {
   constructor() {
